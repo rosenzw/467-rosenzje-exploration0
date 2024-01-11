8fdfaf6c333e283a5e61809b383e2fa94d3d861f
rosenzw <jesseswig@gmail.com>
2024-01-11 12:30:39 -0800

    changed server text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from rosenzje!
+          Automatically Deployed by GitHub for rosenzje!
         </p>
         <a
           className="App-link"
